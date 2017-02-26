e70e92204bc05ec997c36243825b9c9a5b039575
Angus MacDonald <angusamacdonald@hotmail.com>
2017-02-26 00:16:42 -0800

    Styling work
    Added BG images to picutre frames, shrunk fonts and the page in general.

--- src/js/components/decorative/project/template.js
+++ src/js/components/decorative/project/template.js
@@ -5,15 +5,23 @@ export default `
         ng-checked="$ctrl.projectExpanded" 
         name="{{$ctrl.projectGroup}}">
     <label for="project" class="project__title">
-        <img class="project__logo" src="{{$ctrl.projectIcon}}">
+        <div class="project__logo">
+            <img class="project__logo-img" src="{{$ctrl.projectIcon}}">
+        </div>
         <div class="project__name-and-link">
-            <div class="project__name">{{$ctrl.projectTitle}}</div>
-            <a class="project__link"
-               ng-if="$ctrl.projectLink"
-               target="_blank"
+            <div class="project__name">
+                {{$ctrl.projectTitle}}
+                <svg class="project__details-toggle-arrow">
+                    <use xlink:href="img/chevron-right.svg#chevron-right"></use>
+                </svg>
+            </div>
+            <div class="project__link"
+               ng-if="$ctrl.projectLink">
+              <a target="_blank"
                href="{{$ctrl.projectLink}}">
                 Visit
-            </a>
+              </a>
+          </div>
         </div>
     </label>
     <div class="project__details">
