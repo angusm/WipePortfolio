9122689033a27a8d1e703e548ae20038921b03ef
Angus MacDonald <angusamacdonald@hotmail.com>
2017-06-06 22:29:54 -0700

    Expanded page height to allow for breather moments
    When the curtaining effect is applied there will be moments of
    non-transition with scrolling due to increased height.

--- src/js/components/decorative/portfolio-page/portfolio-section/template.js
+++ src/js/components/decorative/portfolio-page/portfolio-section/template.js
@@ -1,7 +1,6 @@
 export default `
 <portfolio-section-outer-container>
-    <portfolio-section-container>
-        <ng-transclude></ng-transclude>
+    <portfolio-section-container ng-transclude>
     </portfolio-section-container>
 </portfolio-section-outer-container>
 `;
